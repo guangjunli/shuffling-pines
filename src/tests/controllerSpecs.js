94d9db7e8ba6cac22e51cc2d2fa1ce6713d1cca7
Guangjun Li <guangjunli@gmail.com>
2015-11-02 06:40:30 -0500

    working on controller unit tests. rearranged where to clear localStorage

--- src/tests/controllerSpecs.js
+++ src/tests/controllerSpecs.js
@@ -27,11 +27,39 @@ describe('Controllers', function() {
   describe('TabController', function() {
     var tabController;
     var formController;
+    var tabScope;
+
+    //Note I had to pull this into its own beforeEach and place it before loading
+    //the module to make the test "should have two known guests to start with" pass
+    beforeEach(function() {
+      if (window && window.localStorage) {
+        window.localStorage.clear();
+        console.log("cleared local storage");
+      }
+    });
+
     beforeEach(angular.mock.module('shuffling'));
-    beforeEach(angular.mock.inject(function($controller) {
+
+    beforeEach(angular.mock.inject(function($controller, $rootScope, storageService) {
+      tabScope = $rootScope.$new();
       formController = $controller('FormController');
-      tabController = $controller('TabController');
+      tabController = $controller('TabController', {$scope : tabScope});
+
     }));
+
+    it('did not put data into $scope', function() {
+      expect(tabScope.guests).toBeUndefined();
+    });
+
+    it('should have two known guests to start with', function() {
+      console.log("checking first two guests");
+      var allGuests = tabController.guests;
+      expect(allGuests.length).not.toBeLessThan(2);
+      var guest1 = allGuests[0];
+      expect(guest1.name).toEqual('First Guest');
+
+    });
+
 
   });
 });
